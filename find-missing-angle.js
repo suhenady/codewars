bcdae1dec2edb2602fec5532c86c70be2baa6986
Jewel Addy <jewel.addy@gmail.com>
2022-10-09 23:34:44 -0400

    new codewars solutions

---
+++ find-missing-angle.js
@@ -0,0 +1,12 @@
+/*You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+https://en.wikipedia.org/wiki/Triangle*/
+
+function otherAngle(a, b) {
+    let thirdAngle = 180 - (Math.abs(a) + Math.abs(b));
+    return thirdAngle
+  }
