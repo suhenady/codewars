3a33fe8213a9a59bd512db5f8743fed70488099b
Jewel Addy <102772399+suhenady@users.noreply.github.com>
2023-02-09 22:49:49 -0500

    first commit

---
+++ return-square.js
@@ -0,0 +1,7 @@
+/* This codewars only passed the test if you named your function 'square'. Now you have to write a function that takes an argument and returns the square of it. */
+
+function square(num){
+    let squared = num ** 2;
+    return squared;
+  }
+
