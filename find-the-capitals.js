e9428edad7fb9daf5509c6cecc079d5a02dd8a60
Jewel Addy <102772399+suhenady@users.noreply.github.com>
2023-03-05 00:23:23 -0500

    first commit

---
+++ find-the-capitals.js
@@ -0,0 +1,12 @@
+/* Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string. */
+
+var capitals = function (word) {
+    let arr = [];
+      for(let i = 0; i < word.length; i++) {
+            if(word[i].toUpperCase() == word[i]) {
+                arr.push(i);
+            }
+        }
+    return arr;
+};
+
